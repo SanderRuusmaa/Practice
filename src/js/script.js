b5c45e1ad0e1e264a865f8106f01c488e9d7c156
SanderRuusmaa <56891593+SanderRuusmaa@users.noreply.github.com>
2020-07-23 17:12:51 +0300

    lazy-loading, small tweaks
    Added lazy-loading to images, fiddled with scrollorama, did not get it to work, small tweaks here and there.

--- src/js/script.js
+++ src/js/script.js
@@ -14,8 +14,33 @@ function handleStepExit(response){
 
 scroller.setup({
     step: '.scrollamaContainer',
-    offset: 0.55
+    offset: 0.67
 
 })
 .onStepEnter(handleStepEnter)
 .onStepExit(handleStepExit)
+
+
+// Lazy-loading
+
+const targets = document.querySelectorAll("img");
+
+const lazyLoad = target => {
+    const io = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+
+            if (entry.isIntersecting){
+                const img = entry.target;
+                const src = img.getAttribute('data-lazy');
+
+                img.setAttribute('src', src);
+
+                observer.disconnect();
+            }
+        });
+    });
+
+    io.observe(target);
+};
+
+targets.forEach(lazyLoad);
